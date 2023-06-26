ad6dcd941c4798d9c4d9c4ebd0657b7eecf6ff20
guisantosalves <workguisantos@gmail.com>
2023-06-26 10:30:01 -0400

    I have made the header

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Header from "@/components/Header";
+import Board from "@/components/Board";
 
 // everything inside the app is redering on the server.
 export default function Home() {
   return (
     <main>
-      {/* header */}
       <Header />
-      {/* body */}
-      <h1>trello clone lets goo</h1>
+      <Board />
     </main>
   )
 }
---
+++ components/Board.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+export default function Board() {
+  return (
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>{/* redereing all the columns */}</div>}
+    //   </Droppable>
+    // </DragDropContext>
+    <div></div>
+  );
+}
