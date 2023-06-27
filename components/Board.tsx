3477ffadd8b86db2459a6c24c1b228f3a684bbf9
guisantosalves <workguisantos@gmail.com>
2023-06-26 21:31:38 -0400

    setting appwrite

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,12 @@
+"use client";
 import * as React from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export default function Board() {
+  React.useEffect(() => {
+    // get board
+  }, []);
+
   return (
     // <DragDropContext>
     //   <Droppable droppableId="board" direction="horizontal" type="column">
